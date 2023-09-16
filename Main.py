99cd41ba018712916efe3970f1daba8121b524f7
Demetre Seturidze <dseturidze25@gmail.com>
2023-09-16 17:13:32 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,17 +12,24 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class system:
     ENGINE = 'gpt-4'
     TOKENS = 2000
+    LANGS = ["English", "French", "Spanish", "Italian", "Portuguese", "Hebrew", "Russian", "German", "Dutch", "Turkish"]
 
     # this returns a new dialog_list with one entry in it: the system command,
-    # which essentially assigns a role to the AI for it to control during the rest
-    # of a dialog.
+    # which essentially assigns a role to the AI during the rest of a dialog.
     def syscommand(prompt):
         return [{'role' : 'system',
                  'content' : prompt}]
 
+    # checks whether or not the given langs are supported, otherwise throws an exception
+    def checklang(*langs):
+        for lang in langs:
+            if (lang not in system.LANGS):
+                raise NotImplementedError(lang + " is not yet supported.")
+    
     # this returns a new dialog_list for a dialog in which the AI is to continuously
     # provide feedback to a native speaker of (firstlang) trying to learn (lang).
     def generate_langcheck(lang, firstlang):
+        system.checklang(lang, firstlang)
         return system.syscommand("You are an expert " + lang + " speaker, you are speaking with a user. The user is trying to learn "
                           + lang + " as it is not their first language. Their first language is " + firstlang + ". Assume they know no other languages unless specified otherwise. Your job is to assess the grammar of the sentences, "
                           "and grade it on a scale of 1 to 100. Go sentence by sentence making sure to grade each one "
@@ -31,6 +38,7 @@ class system:
     # this returns a new dialog_list for a dialog in which the AI is to continuously
     # converse in (lang) with a native speaker of (firstlang).
     def generate_conversation(lang, firstlang):
+        system.checklang(lang, firstlang)
         return system.syscommand("You are an expert " + lang + " speaker, you are speaking with a user. The user is trying to learn "
                  + lang + " as it is not their first language. Their first language is " + firstlang + ". Assume they know no other languages unless specified otherwise. Your job is to converse with them in " + lang + " while covertly "
                  "keeping track of their fluency level and adjusting your own " + lang + " level to cater to the user's ability."
@@ -41,6 +49,7 @@ class system:
     # this will be used for translating the built-in introduction phrases if the first language of the user 
     # is not English.
     def generate_translator(lang):
+        system.checklang(lang)
         return system.syscommand("You are an expert " + lang + " speaker, and your job is to translate the English phrases with which you are provided into " + lang + " as accurately as possible.")
 
 
@@ -110,19 +119,11 @@ class user:
             self.translator = dialog.translator(first_language)
 
     # creates a new langcheck in (lang).
-    # throws an exception if you try to langcheck with your first language.
     def _new_langcheck(self, lang):
-        if (lang == self.first_language):
-            raise SystemError()
-        
         self.langchecks[lang] = dialog.langcheck(lang, self.first_language)
     
     # creates a new conversation in (lang).
-    # throws an exception if you try to converse in your first language.
     def _new_conversation(self, lang):
-        if (lang == self.first_language):
-            raise SystemError()
-        
         self.conversations[lang] = dialog.conversation(lang, self.first_language)
       
     # provides langcheck feedback to one sentence in (lang).
@@ -156,7 +157,7 @@ class user:
         else:
             verb = "Continue"
         
-        self._print_native(verb + " your conversation in " + lang + "!")
+        self._print_native(verb + " your conversation in " + lang + "!\nEnter the letter X at any point to end the dialog.")
 
         userinput = self._input()
 
@@ -170,7 +171,7 @@ class user:
         if (lang not in self.langchecks):
             self._new_langcheck(lang)
           
-        self._print_native("Write a sentence in " + lang + "!")
+        self._print_native("Write a sentence in " + lang + "!\nEnter the letter X at any point to end the dialog.")
 
         userinput = self._input()
 
@@ -184,12 +185,4 @@ if __name__ == "__main__":
     
     Deme = user("Deme")
 
-    Deme.hold_conversation("French")
-
-
-
-
-    #sentence = "Esto oracion no es escribo correctitud"
-    #language = "Spanish"
-    #result = assess_grammar(sentence, language)
-    #print(f"In {language}, the assessment is: {result}")
+    Deme.hold_conversation("Spanish")
