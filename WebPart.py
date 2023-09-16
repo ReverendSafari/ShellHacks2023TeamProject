71c3fdd9eaa77ac0a6b8fee0c09610dd29fc9ee4
ReverendSafari <s.swanson24@ncf.edu>
2023-09-16 15:48:31 -0400

    Login WORKING
    Make the chat not available until the user successfully logs in

--- WebPart.py
+++ WebPart.py
@@ -1,47 +1,33 @@
 import streamlit as st
 import sqlite3
 
-# Initialize SQLite DB
 # Initialize SQLite database
 def init_db():
-    conn = sqlite3.connect("users.db")
-    c = conn.cursor()
-    c.execute(
-        """CREATE TABLE IF NOT EXISTS users (
-            username TEXT PRIMARY KEY,
-            password TEXT NOT NULL
-        );"""
-    )
-    conn.commit()
-    conn.close()
+    try:
+        with sqlite3.connect("users.db") as conn:
+            c = conn.cursor()
+            c.execute(
+                """CREATE TABLE IF NOT EXISTS users (
+                    username TEXT PRIMARY KEY,
+                    password TEXT NOT NULL
+                );"""
+            )
+            conn.commit()
+    except sqlite3.OperationalError as e:
+        if str(e) == "database is locked":
+            with sqlite3.connect("users.db") as conn:
+                conn.cursor().execute("SELECT * from users LIMIT 1;")
+            init_db()
 
 init_db()
-
-# Check if we are on the registration page or login page
-query_params = st.experimental_get_query_params()
-page = query_params.get("page", ["login"])[0]
-
-# Sidebar
-st.sidebar.title('LangGPT')
-
-# Organization for the UI
-col1, col2, col3 = st.columns(3)
-
-# Getting the buttons inline for better visibility
-with col1:
-    ntLang = st.selectbox('Native language', ['english', 'spanish', 'french'])
-
-with col2:
-    tgLang = st.selectbox('Target Language', ['english', 'spanish', 'french'])
-
-with col3:
-    name = st.text_input('Enter a name for the bot')
-
-user_input = st.text_input('Start your conversation here')
-
 # Initialize session state
 if 'page' not in st.session_state:
     st.session_state.page = "login"
+if 'is_logged_in' not in st.session_state:  # Initialize login state
+    st.session_state.is_logged_in = False
+
+# Sidebar
+st.sidebar.title('LangGPT')
 
 # Sidebar button nav
 if st.session_state.page == "login":
@@ -49,7 +35,7 @@ if st.session_state.page == "login":
 elif st.session_state.page == "register":
     st.sidebar.button("Go to Login", on_click=lambda: setattr(st.session_state, "page", "login"), key='logButton')
 
-#Login State
+# Login State
 if st.session_state.page == "login":
     st.sidebar.title("Login")
     username = st.sidebar.text_input("Username")
@@ -60,13 +46,12 @@ if st.session_state.page == "login":
         c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
         if c.fetchone():
             st.success("Logged in successfully")
-            # set other session states or perform other actions here
+            st.session_state.is_logged_in = True  # Update login state
         else:
             st.error("Invalid credentials")
         conn.close()
-    
 
-#Registration State
+# Registration State
 elif st.session_state.page == "register":
     st.sidebar.title("Register")
     new_username = st.sidebar.text_input("New Username")
@@ -82,3 +67,23 @@ elif st.session_state.page == "register":
         except sqlite3.IntegrityError:  # Username already exists
             st.sidebar.error("Username already exists. Please choose another.")
         conn.close()
+
+# Organization for the UI
+col1, col2, col3 = st.columns(3)
+
+# Getting the buttons inline for better visibility
+with col1:
+    ntLang = st.selectbox('Native language', ['english', 'spanish', 'french'])
+
+with col2:
+    tgLang = st.selectbox('Target Language', ['english', 'spanish', 'french'])
+
+with col3:
+    name = st.text_input('Enter a name for the bot')
+
+# Show the chat feature only if the user is logged in
+if st.session_state.is_logged_in:
+    st.title("Chat Feature")
+    user_input = st.text_input('Start your conversation here')
+else:
+    st.warning("Please log in to access the chat feature")
